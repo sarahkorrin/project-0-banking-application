9b8d81a8bafd69e57610a5a293656e8f442ffbc8
sarahkorrin <sarahkorrin@tutanota.com>
2022-03-02 02:23:03 -0500

    "test"

---
+++ src/my/bankapp/BankRunnerMain.java
@@ -0,0 +1,12 @@
+package my.bankapp;
+
+public class BankRunnerMain
+{
+    public static void main(String[] args) {
+
+        MenuOptions mainMenuOptions = new MenuOptions();
+        mainMenuOptions.welcomeMenu();
+
+        // Hello Revature :)
+    }
+}
